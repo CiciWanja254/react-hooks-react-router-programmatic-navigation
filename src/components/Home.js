66384c45b21cc38c3040f09f6f6a0ea1ed32c31d
Cici254 <thukucindy00@gmail.com>
2023-05-01 17:01:46 +0300

    Complete assignmnet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 
 function Home({ isLoggedIn }) {
+  const history = useHistory();
+
   if (!isLoggedIn) return <Redirect to="/login" />;
+
+  function generateNewItemId() {
+    // generate a timestamp as our ID
+    const timestamp = Date.now().toString(16);
+    return timestamp;
+  }
+
+  function handleCreateItem() {
+    // code to create new item
+    const newItemId = generateNewItemId();
+    console.log("New Item Created Successfully");
+    // navigate to detail page for newly created item
+    history.push(`/item/${newItemId}`);
+  }
 
   return (
     <div>
       <h1>Home!</h1>
+      <button onClick={handleCreateItem}>Create Item</button>
     </div>
   );
 }
